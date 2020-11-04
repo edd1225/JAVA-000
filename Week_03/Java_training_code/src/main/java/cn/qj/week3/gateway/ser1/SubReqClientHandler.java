a0cd62be1c9b11adf52fc0638f2bb4b760a17430
edd1225 <123567549@qq.com>
2020-11-04 16:58:51 +0800

    week3

---
+++ Week_03/Java_training_code/src/main/java/cn/qj/week3/gateway/ser1/SubReqClientHandler.java
@@ -0,0 +1,48 @@
+package cn.qj.week3.gateway.client;
+
+import cn.qj.week3.gateway.server.codes.pojo.SubscribeReq;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class SubReqClientHandler extends ChannelHandlerAdapter {
+
+    /**
+     * Creates a client-side handler.
+     */
+    public SubReqClientHandler() {
+    }
+
+
+    public void channelActive(ChannelHandlerContext ctx) {
+	for (int i = 0; i < 10; i++) {
+	    ctx.write(subReq(i));
+	}
+	ctx.flush();
+    }
+
+    private SubscribeReq subReq(int i) {
+	SubscribeReq req = new SubscribeReq();
+	req.setAddress("hangzhou ");
+	req.setPhoneNumber("1380000000");
+	req.setProductName("Nettytest");
+	req.setSubReqID(i);
+	req.setUserName("qianjiang");
+	return req;
+    }
+
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+	    throws Exception {
+	System.out.println("Receive server response : [" + msg + "]");
+    }
+
+
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+	ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+	cause.printStackTrace();
+	ctx.close();
+    }
+}
---
+++ Week_03/Java_training_code/src/main/java/cn/qj/week3/gateway/server/NettyServerApp.java
@@ -0,0 +1,29 @@
+package cn.qj.week3.gateway.server;
+
+
+import cn.qj.week3.gateway.server.inbound.HttpInboundServer;
+
+
+/** 主要启动类
+ *
+ * Netty 服务端
+ *  @Author edd1225(qianjiang)
+ *  @Date 2020/11/03 上午
+ *
+ */
+public class NettyServerApplication {
+
+    public final static String PROPERTIES_PATH = "/META-INF/HttpApplication.properties";
+
+    public static void main(String[] args) {
+        HttpInboundServer server = new HttpInboundServer(PROPERTIES_PATH);
+        try {
+            server.run();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
