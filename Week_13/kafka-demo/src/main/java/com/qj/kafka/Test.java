7cf10ea64d681986279be1ef8150945c2b766473
edd1225 <edd1225@users.noreply.github.com>
2021-02-06 21:28:04 +0800

    Update Test.java

--- Week_13/kafka-demo/src/main/java/com/qj/kafka/Test.java
+++ Week_13/kafka-demo/src/main/java/com/qj/kafka/Test.java
@@ -6,6 +6,6 @@ public class Test {
 
     public static void main(String[] args) {
         //17
-        System.out.println(Math.abs("gp-gid3".hashCode())%50);
+        System.out.println(Math.abs("gid3".hashCode())%50);
     }
 }
